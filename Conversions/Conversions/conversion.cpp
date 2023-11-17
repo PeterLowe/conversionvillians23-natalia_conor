511581fc1e519d7c4a6ffcf9479528778bacd529
(Student  C00294480) Natalia Ryl <C00294480@setu.ie>
2023-11-17 10:11:44 +0000

    A.angle

--- Conversions/Conversions/conversion.cpp
+++ Conversions/Conversions/conversion.cpp
@@ -3,12 +3,22 @@
 
 #include <iostream>
 
+char printMenu();
+void degreesToRadians();
+
 int main()
 {
+	char userChoice; // letter for user to pick conversion
+	userChoice = printMenu(); 
+
+	if (userChoice == 'A')
+	{
+		degreesToRadians();
+	}
 	return 1;
 }
 
-char printMenu() // natalia
+char printMenu() // natalia & conor
 {
 	char choice = ' '; // user input choice
 	std::cout << "Please enter a upper case letter to convert from left to right and lower to convert from right to left" << std::endl;
@@ -24,4 +34,17 @@ char printMenu() // natalia
 	std::cin >> choice;
 
 	return choice;
+}
+
+void degreesToRadians() // natalia
+{
+	float degrees = 0.0f;
+	float radians = 0.0f;
+
+	std::cout << "Enter degrees: " << std::endl;
+	std::cin >> degrees;
+
+	radians = degrees * 0.0175;
+
+	std::cout << "That is " << radians << " radians" << std::endl;
 }
