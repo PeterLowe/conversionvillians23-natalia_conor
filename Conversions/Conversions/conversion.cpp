d210f07a90998ce7cab8d1b3781ff755b8c4189c
(Student  C00294480) Natalia Ryl <C00294480@setu.ie>
2023-11-17 09:48:19 +0000

    printmenu done

--- Conversions/Conversions/conversion.cpp
+++ Conversions/Conversions/conversion.cpp
@@ -1,6 +1,23 @@
 // Natalia Ryl
 
+#include <iostream>
+
 int main()
 {
 	return 1;
+}
+
+char printMenu() // natalia
+{
+	char choice = ' '; // user input choice
+	std::cout << "Please enter a upper case letter to convert from left to right and lower to convert from right to left" << std::endl;
+	std::cout << "[A] Degrees <---> Radians [a]" << std::endl;
+	std::cout << "[B] Centimeters <---> Feet & Inches [b]" << std::endl;
+	std::cout << "[C] Litres <---> Gallons [c]" << std::endl;
+
+	std::cout << "[Q] Exit [---] Exit [q]" << std::endl;
+	std::cout << "Enter your Choice: ";
+	std::cin >> choice;
+	
+	return choice;
 }
